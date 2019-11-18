d7a90bad5689fd4b9c8f6378710f193728dd69ef
jamesbellnet <james@jamesbell.net>
2019-11-18 13:40:45 +0000

    Merge template updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = {
-        plugins: [
-            require('autoprefixer')
-        ]
-    }
+module.exports = {
+    plugins: [
+        require('postcss-preset-env')
+    ]
 }
